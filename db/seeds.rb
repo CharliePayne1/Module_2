5ab3e5d45742f6f3d9318484c4bc6c3d42fb9109
CharliePayne1 <charliepayne1101@gmail.com>
2020-05-23 11:45:28 +0100

    database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Receipt.destroy_all
+BankAccount.destroy_all
+Retailer.destroy_all
+Customer.destroy_all
+
+30.times do
+    Customer.create({
+        name: Faker::Name.name,
+        age: (16..100).to_a.sample,
+        occupation: Faker::Company.profession
+    })
+end
+
+20.times do
+    Retailer.create({
+     sector: Faker::Job.field,
+     annual_turnover: Faker::Number.between(from: 20000, to: 1100000),
+     established: Faker::Number.between(from: 1930, to: 2020),
+     employees: Faker::Number.between(from: 20, to: 10000)
+ })
+ end
+
+35.times do
+    BankAccount.create({
+    account_number: Faker::Number.between(from: 10000000, to: 99999999),
+    sort_code: Faker::Number.between(from: 100000, to: 999999),
+    type_of_account: ['current', 'saving', 'saving'].sample,
+    funds: Faker::Number.between(from: 0, to: 10000),
+    overdraft: [2000, 1500, 1000, 750, 500, 250, 0].sample,
+    customer_id: Customer.all.sample.id
+    })
+end
+
+100.times do
+    Receipt.create({
+        total_amount: Faker::Number.between(from: 3, to: 10000),
+        items: ['shoes', 'handbag', 'mobile', 'groceries', 'bedsit', 'lava lamp', 'home exercise kit', 'banana bread', 'Tiger King DVD', 'Oyster Card'].sample,
+        bank_account_id: BankAccount.all.sample.id,
+        retailer_id: Retailer.all.sample.id
+    })
+end
+
+# Receipt.create(total_amount: 50, items: “shoes”, bank_account_id: 20, retailer_id: 5
+# account_number: Faker::Bank.account_number(digits: 8),
+# sort_code: Faker::Bank.sort_code(digits: 6),
+#bank_account_id: Faker::Number.within(range: 1..35),
+#retailer_id: Faker::Number.within(range: 1..20)
+#Faker::Number.between(from: 211, to: 272)
+#customer_id: Faker::Number.within(range: 1..30)
+#customer_id: Customer.all.sample
+#BankAccount.create(account_number: 12345678, sort_code: 123456, type_of_account: “pls work”, funds: 404, overdraft: 500, customer_id: 15)
