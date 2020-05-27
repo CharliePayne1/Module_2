edbb26a5493f4b51f6d951fb26d39c3c4672bd85
github username <github email address>
2020-05-27 09:50:11 +0100

    receipt show page updated

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -3,3 +3,16 @@ class BankAccount < ApplicationRecord
     has_many :receipts
     has_many :retailers, through: :receipts
 end
+
+# Show page 
+# available_funds - funds + overdraft
+# big_spender customer occupation with highest available funds
+# Delete the bank account 
+# View your 10 most recent transactions link to most recent receipts 
+# Where have they spent the most money?
+# Average transaction value
+# Average basket size 
+# Link to customer profile 
+# Edit the overdraft (increasing) & available funds by paying money in/ withdrawing money 
+# Overdraft can only increase
+# Available funds can increase/decrease 
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,4 +1,25 @@
 class Receipt < ApplicationRecord
     belongs_to :bank_account
     belongs_to :retailer
+
+    def customer_first_name
+        self.bank_account.customer.first_name
+    end
+
+    def customer_last_name
+        self.bank_account.customer.last_name 
+    end
+
+    def retailer_name
+        self.retailer.name 
+    end
+
+    def account_type
+        self.bank_account.type_of_account
+    end
+
+    def item_list
+        self.items.split
+    end
+
 end
