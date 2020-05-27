26fa330cc19f4f7b6a051bed2bf32037c55981e0
minamalaj <mina.malaj@hotmail.com>
2020-05-27 14:24:25 +0100

    styling

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,15 +8,39 @@ class Customer < ApplicationRecord
     validates :last_name, presence: true 
     validates :age, numericality: { greater_than: 16 }
     
-    before_save :uppercase_name
+    # before_save :uppercase_name
     
-    def uppercase_name
-        self.first_name = self.first_name.uppercase
-        self.last_name = self.last_name.uppercase
-    end 
+    # def uppercase_name
+    #     self.first_name = self.first_name.uppercase
+    #     self.last_name = self.last_name.uppercase
+    # end 
     
     def full_name 
         "#{last_name}, #{first_name}" 
     end 
 
+    def account_types
+        self.bank_accounts.select {|ba| ba.type_of_account}
+    end 
+
+    def account
+        self.bank_accounts.type_of_account
+    end 
+
+    def my_funds
+        self.bank_accounts.funds
+    end 
+
+    def my_overdraft
+        self.bank_accounts.overdraft
+    end 
+
+    def my_account_number
+        self.bank_accounts.account_number
+    end 
+
+    def my_sort_code
+        self.bank_accounts.sort_code
+    end 
+    
 end
