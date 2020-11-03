26a4607adaed6565ca2fbd173c4d93c96c8706af
minamalaj <mina.malaj@hotmail.com>
2020-11-03 15:47:10 +0000

    //

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("jquery")
-require("materialize-sprockets")
-require("chartkick")
-require("chart.js")
+require("jquery")
+// require("materialize-sprockets")
+// require("chartkick")
+// require("chart.js")
 
 
 $( document ).on('turbolinks:load', function() {
